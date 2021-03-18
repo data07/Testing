6fb278b1a74c4b5a43f0a83a2a4939d319c4f0bb
data07 <darren.data07@gmail.com>
2021-03-18 09:02:33 +0800

    Add files via upload

---
+++ Main.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace InterValue
+{
+    partial class Main
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.txtJSON = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // txtUser
+            // 
+            this.txtUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtUser.Location = new System.Drawing.Point(12, 136);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(320, 31);
+            this.txtUser.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(80, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(181, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Email / Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(103, 196);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password:";
+            // 
+            // txtPass
+            // 
+            this.txtPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtPass.Location = new System.Drawing.Point(12, 241);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.PasswordChar = '@';
+            this.txtPass.Size = new System.Drawing.Size(320, 31);
+            this.txtPass.TabIndex = 2;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnLogin.Location = new System.Drawing.Point(40, 336);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(221, 52);
+            this.btnLogin.TabIndex = 5;
+            this.btnLogin.Text = "LOG IN";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // txtJSON
+            // 
+            this.txtJSON.Location = new System.Drawing.Point(354, 57);
+            this.txtJSON.Name = "txtJSON";
+            this.txtJSON.Size = new System.Drawing.Size(498, 331);
+            this.txtJSON.TabIndex = 6;
+            this.txtJSON.Text = "";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(873, 413);
+            this.Controls.Add(this.txtJSON);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtUser);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "InterValue Technical Exam";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.RichTextBox txtJSON;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Linq;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Newtonsoft.Json.Serialization;
+using System.Text.Json;
+
+namespace InterValue
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnLogin_Click(object sender, EventArgs e)
+        {
+            var options = new ChromeOptions();
+            options.LeaveBrowserRunning = false;
+            IWebDriver driver;
+            ChromeDriverService service = ChromeDriverService.CreateDefaultService();
+            service.HideCommandPromptWindow = true;
+            options.AddArguments("headless");
+            using (driver = new ChromeDriver(service, options))
+            {
+                driver.Url = "https://github.com/login";
+                IWebElement username = driver.FindElement(By.Id("login_field"));
+                //Enter your user name
+                username.SendKeys(txtUser.Text);
+                //Locate
+                IWebElement password = driver.FindElement(By.Id("password"));
+                //enter password
+                password.SendKeys(txtPass.Text);
+                driver.FindElement(By.Name("commit")).Click();
+
+                //audit-log-search
+                driver.Url = "https://github.com/settings/security-log";
+                IWebElement auditlog = driver.FindElement(By.Id("audit-log-search"));
+                IWebElement getBox = auditlog.FindElement(By.ClassName("Box"));
+ 
+                var getUser = getBox.FindElements(By.ClassName("member-username"));
+      
+                var AccountID = getUser.First().Text;
+                string strJSON = "{" + Convert.ToChar(34) + "Account" + Convert.ToChar(34) + ":" + Convert.ToChar(34) + AccountID + Convert.ToChar(34) + "," + Convert.ToChar(34) + "Logs"  +Convert.ToChar(34) + ":[";
+                var getAllEvents = getBox.FindElements(By.XPath("//span[contains(@class, 'audit-type')]/*[@href]"));
+                var getAllTime = getBox.FindElements(By.XPath("//div/*[@datetime]"));
+
+                string joinedString = "";
+                for (int i =0; i<=getAllEvents.Count-1;i++)
+                {
+                    if (joinedString!="") { joinedString += ","; }
+                    joinedString = joinedString + "{" + Convert.ToChar(34) + "event" + Convert.ToChar(34) + ":" + Convert.ToChar(34) + getAllEvents[i].Text + Convert.ToChar(34) + "," + Convert.ToChar(34) + "time" + Convert.ToChar(34) + ":" + Convert.ToChar(34) + getAllTime[i].Text + Convert.ToChar(34) + "}";
+
+
+                }
+  
+                txtJSON.Text = strJSON + joinedString + "]}";
+            }
+        }
+    }
+}
